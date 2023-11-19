33cb2f3439bc3d550dd8e85f2173f56cf70c5996
MDoan75 <141285754+MDoan75@users.noreply.github.com>
2023-11-19 10:51:58 -0500

    Update Index.js

--- Index.js
+++ Index.js
@@ -4,67 +4,72 @@ $(document).ready(function() {
   var interval;
   var timeLeft = 10;
   var score = 0;
-  var randomNumberGenerator = function(size) {
-    return Math.ceil(Math.random() * size);
-  }
 
-  var questionGenerator = function() {
-    var question = {};
-    var num1 = randomNumberGenerator(10);
-    var num2 = randomNumberGenerator(10);
-
-    question.answer = num1 + num2;
-    question.equation = String(num1) + " + " + String(num2);
-
-    return question;
-  }
-
-  var renderNewQuestion = function() {
-    currentQuestion = questionGenerator();
-    $('#equation').text(currentQuestion.equation);
-  }
-  var checkAnswer = function(userInput, answer) {
-    if (userInput === answer) {
-      renderNewQuestion();
-      $('#user-input').val('');
-      updateTimeLeft(+1);
-    }
-  }
-  var startGame = function() {
+  var updateTimeLeft = function (amount) {
+    timeLeft += amount;
+    $('#time-left').text(timeLeft);
+  };
+  
+  var updateScore = function (amount) {
+    score += amount;
+    $('#current-score').text("Current score: " + score);
+  };
+  
+  var startGame = function () {
     if (!interval) {
-      console.log(timeLeft);
       if (timeLeft === 0) {
         updateTimeLeft(10);
+        updateScore(-score);
+       $('#show-seconds').css('color', 'black');
       }
-      interval = setInterval(function() {
+      interval = setInterval(function () {
         updateTimeLeft(-1);
+        if (timeLeft < 4) {
+          $('#show-seconds').css('color', 'green');
+        } else if (timeLeft <= 7) {
+          $('#show-seconds').css('color', 'blue');
+        } else 
         if (timeLeft === 0) {
           clearInterval(interval);
           interval = undefined;
         }
-      }, 1000);
+      }, 1000);  
+      
     }
-  }
-  $('#user-input').on('keyup', function() {
+  
+  var randomNumberGenerator = function (size) {
+    return Math.ceil(Math.random() * size);
+  };
+  
+  var questionGenerator = function () {
+    var question = {};
+    var num1 = randomNumberGenerator(10);
+    var num2 = randomNumberGenerator(10);
+    
+    question.answer = num1 + num2;
+    question.equation = String(num1) + " + " + String(num2);
+    
+    return question;
+  };
+  
+  var renderNewQuestion = function () {
+    currentQuestion = questionGenerator();
+    $('#equation').text(currentQuestion.equation);  
+  };
+  
+  var checkAnswer = function (userInput, answer) {
+    if (userInput === answer) {
+      renderNewQuestion();
+      $('#user-input').val('');
+      updateTimeLeft(+1);
+      updateScore(+1);
+    }
+  };
+  
+  $('#user-input').on('keyup', function () {
     startGame();
     checkAnswer(Number($(this).val()), currentQuestion.answer);
   });
+  
   renderNewQuestion();
-
-  var interval = setInterval(function() {
-    updateTimeLeft(-1);
-    if (timeLeft === 0) {
-      clearInterval(interval);
-      interval = undefined;
-    }
-  }, 1000);
-
-  var updateTimeLeft = function(amount) {
-    timeLeft += amount;
-    $('#time-left').text(timeLeft);
-  };
-  var updateScore = function (amount) {
-    score += amount;
-    $('#score').text(score);
-  };
 });
